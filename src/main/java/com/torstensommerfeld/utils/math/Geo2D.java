c96bb9f4de3abae8ce9b070dc03663ff2a5db33c
storsten <torsten@loveholidays.com>
2018-04-28 21:36:34 +0100

    remove spaces at the end of lines

--- src/main/java/com/torstensommerfeld/utils/math/Geo2D.java
+++ src/main/java/com/torstensommerfeld/utils/math/Geo2D.java
@@ -11,7 +11,7 @@ import com.torstensommerfeld.utils.math.shapes.QuadraticFunctionParameters;
 
 /**
  * This class contains a set of methods to perform various calculations to solve problems within 2D domain
- * 
+ *
  * @author torsten
  *
  */
@@ -33,7 +33,7 @@ public class Geo2D {
 
     /**
      * Compared to getDistance this method does not take the square root and hence it is faster
-     * 
+     *
      * @return square of distance of point (x1, y1) to point (x2, y2)
      */
     public static double getDistanceSqr(double x1, double y1, double x2, double y2) {
@@ -42,7 +42,7 @@ public class Geo2D {
 
     /**
      * Compared to getDistance this method does not take the square root and hence it is faster
-     * 
+     *
      * @return square of distance of point (x1, y1) to point (x2, y2)
      */
     public static double getDistanceSqr(int x1, int y1, int x2, int y2) {
@@ -50,7 +50,7 @@ public class Geo2D {
     }
 
     /**
-     * 
+     *
      * @return distance minimum distance of point (x, y) to line defined by the points (x1, y2) and (x2, y2)
      */
     public static double distanceOfPointToLine(int x, int y, int x1, int y1, int x2, int y2) {
@@ -58,7 +58,7 @@ public class Geo2D {
     }
 
     /**
-     * 
+     *
      * @return distance minimum distance of point (x, y) to line defined by the points (x1, y2) and (x2, y2)
      */
     public static double distanceOfPointToLine(int x, int y, int x1, int y1, int x2, int y2, double distanceP1P2) {
@@ -78,18 +78,18 @@ public class Geo2D {
         /*-
          *   x2-x1      x1    y1 - y2      x
          *  (y2-y1) v + y1 = (x2 - y1) w + y
-         *  
+         *
          *  dx = x2 - x1
          *  dy = y2 - y1
-         * 
+         *
          * I )   v*dx + x1 = -w*dy + x | * dy
          * II)   v*dy + y1 =  w*dx + y | * dx
-         * 
+         *
          * I ')  v*dx*dy + x1*dy = -w*dy*dy + x*dy
          * II')  v*dy*dx + y1*dx =  w*dx*dx + y*dx
-         * 
+         *
          * III: I' - II') x1*dy - y1*dx = -w*dy*dy + x*dy - w*dx*dx - y*dx
-         * 
+         *
          * III'   ) x1*dy - y1*dx = -w*dy*dy - w*dx*dx + x*dy - y*dx
          * III''  ) x1*dy - y1*dx = -w*(dy*dy + dx*dx) + x*dy - y*dx
          * III''' ) w*(dy*dy + dx*dx) = x*dy -y*dx - x1*dy + y1*dx
@@ -108,13 +108,13 @@ public class Geo2D {
 
     /**
      * This method calculates and returns f for the given equation:
-     * 
+     *
      * x1 + (x2-x1)f = x2 - (x2-x1)(1-f) = x
-     * 
+     *
      * y1 + (y2-y1)f = y2 - (y2-y1)(1-f) = y
-     * 
+     *
      * f = (y - y1) / (y2-y1) and f = (x - x1) / (x2-x1)
-     * 
+     *
      * The method assumes that point (x,y) is between (x1,y1) and (x2,y2)
      */
 
@@ -130,7 +130,7 @@ public class Geo2D {
 
     /**
      * This method calculates the parameters of a circle (center, radius) based on 3 points.
-     * 
+     *
      * @param x1
      * @param y1
      * @param x2
@@ -143,45 +143,45 @@ public class Geo2D {
     public static Circle getCircle(int x1, int y1, int x2, int y2, int x3, int y3, Circle target) {
         /*-
          * x^2 + y^2 + D * x + E * y + F = 0
-         * 
+         *
          * I   ) x1*D + y1*E + F + x1^2 + y1^2 = 0
          * II  ) x2*D + y2*E + F + x2^2 + y2^2 = 0
          * III ) x3*D + y3*E + F + x3^2 + y3^2 = 0
-         * 
+         *
          * c1 = x1^2 + y1^2
          * c2 = x2^2 + y2^2
          * c3 = x3^2 + y3^2
-         * 
+         *
          * I   ') x1*D + y1*E + F + c1 = 0
          * II  ') x2*D + y2*E + F + c2 = 0
          * III ') x3*D + y3*E + F + c3 = 0
-         * 
+         *
          * IV = I - II ) (x1 - x2)D + (y1 - y2)E + c1 - c2 = 0
          *  V = I - III) (x1 - x3)D + (y1 - y3)E + c1 - c3 = 0
-         * 
+         *
          * d1 = x1 - x2
          * e1 = y1 - y2
          * f1 = c1 - c2
-         * 
+         *
          * d2 = x1 - x3
          * e2 = y1 - y3
          * f2 = c1 - c3
-         * 
+         *
          * IV ') d1D + e1E + f1 = 0
          *  V ') d2D + e2E + f2 = 0
-         *  
+         *
          * VI =  IV * d2 - V * d1:
          *    =  e1*d2*E - e2*d1*E + f1 * d2 - f2 * d1
          *   <=> e1*d2*E - e2*d1*E = f2 * d1 - f1 * d2
          *  E = (f1 * d2 - f2 * d1) / (e2*d1 - e1 * d2)
-         *       
+         *
          * E in IV')
          *   d1D + e1E + f1 = 0
          *   d1D = -e1E - f1
          *   D   = ()-e1E - f1) / d1;
-         *   
+         *
          * E and D in I)
-         *  
+         *
          *  x1*D + y1*E + F + x1^2 + y1^2 = 0
          *  -F = x1*D + y1*E + x1^2 + y1^2
          */
@@ -225,11 +225,11 @@ public class Geo2D {
      * AX^2 + BXY + CY^2 + DX + EY + F = 0
      * where X and Y are coordinates of the 5 points
      * and A,B,C,D,E,F are the the general equation's coefficients
-     * 
+     *
      * Because any multiple of the equation describes the same ellipse we can divide the equation by A and still describe the same ellipse
      * X^2 + B'XY + C'Y^2 + D'X + E'Y + F' = 0
      * B'XY + C'Y^2 + D'X + E'Y + F' = -X^2 (*A/A)
-     * 
+     *
      * This makes A=1 and reduced the number of unknowns to 5
      */
     public static Ellipse getEllipse(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4, double x5, double y5, Ellipse target) {
@@ -277,18 +277,18 @@ public class Geo2D {
 
         // calculate ellipse parameters
         /*-
-         * calculating (center) x and (center) y from A,B,C,E and F 
-         * 
+         * calculating (center) x and (center) y from A,B,C,E and F
+         *
          * I)  D = -2Ax - 1By
          * II) E = -1Bx - 2Cy
          *
          * I ') I  * 1B <=> 1BD = -2ABx - 1BBy
          * II') II * 2A <=> 2AE = -2ABX - 4BCy
-         * 
+         *
          * III ) I' - II' <=> 1BD - 2AE = -1BBy + 4BCy
          * III' )             1BD - 2AE = (-1BB + 4BC)y
          * III'')              y = (1BD - 2AE) / (-1BB + 4AC)
-         * 
+         *
          *  y in I:
          *  I') D = -2Ax - 1By
          *  I'') 2Ax = -1By - D
@@ -320,15 +320,15 @@ public class Geo2D {
 
     /**
      * This method can be used if the center and 3 additional points are known
-     * 
+     *
      * The regular ellipse equation degrates to (D and E are defined by the center):
-     * 
+     *
      * B*xi*yi + C*yi*yi + (-2*xc - B*yc)*xi + (-B*xc - 2 * C * yc)*yi + F = -xi*xi
-     * 
+     *
      * <=>
-     * 
+     *
      * B*(xi*yi-yc*xi-xc*yi) + C*(yi*yi - 2*yc*yi) + F = -xi*xi + 2*xc*xi
-     * 
+     *
      * @param target
      * @return
      */
@@ -383,9 +383,9 @@ public class Geo2D {
 
     /**
      * The quadratic equation has the form of ax^2 + bx + c = y
-     * 
+     *
      * This method finds the parameter a, b and c fitting the 3 given points
-     * 
+     *
      * Note: This method calculates it manually. Alternatively we cold solve the linear equation system with 3 unknowns. It gives the same results but would be slower.
      */
     public static QuadraticFunctionParameters getQuadraticFunctionParameters(double x1, double y1, double x2, double y2, double x3, double y3, QuadraticFunctionParameters target) {
@@ -393,40 +393,40 @@ public class Geo2D {
          * I  ) ax1*x1 + b*x1 + c = y1
          * II ) ax2*x2 + b*x2 + c = y2
          * III) ax3*x3 + b*x3 + c = y3
-         * 
+         *
          * IV: I  - III) a(x1*x1 - x3*x3) + b(x1 - x3) = y1 - y3
          * V:  II - III) a(x2*x2 - x3*x3) + b(x2 - x3) = y2 - y3
-         * 
+         *
          * xx13 = x1*x1 - x3*x3
          * x13 = x1 - x3
          * xx23 = x2*x2 - x3*x3
          * x23 = x2 - x3
          * y13 = y1 - y3
          * y23 = y2 - y3
-         * 
+         *
          * IV' ) a*xx13 + b*x13 = y13  | * xx23
          * V'  ) a*xx23 + b*x23 = y23  | * xx13
-         * 
+         *
          * IV'') a*xx13 * xx23 + b*x13 * xx23 = y13 * xx23
          * V'' ) a*xx23 * xx13 + b*x23 * xx13 = y23 * xx13
-         * 
+         *
          * VI: IV'' - V'') b*x13 * xx23 - b*x23 * xx13 = y13 * xx23 - y23 * xx13
          * VI' ) b(x13 * xx23 - x23 * xx13) = y13 * xx23 - y23 * xx13
-         * 
+         *
          * x' = x13 * xx23 - x23 * xx13
          * y' = y13 * xx23 - y23 * xx13
-         * 
+         *
          * VI'' ) b * x' = y'
          * VI''') b = 'y / x'
-         * 
+         *
          * VII: b in IV') a*xx13 + b*x13 = y13
          * VII') a*xx13 = y13 - b*x13
          *       a = (y13 - b*x13) / xx13;
-         * 
+         *
          * VII: b in V') a*xx23 + b*x23 = y23
          * VII') a*xx23 = y13 - b*x23
          *       a = (y23 - b*x23) / xx23;
-         * 
+         *
          */
         double xx13 = x1 * x1 - x3 * x3;
         double x13 = x1 - x3;
@@ -452,13 +452,13 @@ public class Geo2D {
 
     /**
      * returns the solution of a*x^2 + b*x + c = 0
-     * 
+     *
      * returns [NaN, NaN] if there is no solution
-     * 
+     *
      * returns [x, NaN] if there is only a double solution
-     * 
+     *
      * returns [x1, x2] if there are two solutions
-     * 
+     *
      */
     public static double[] solveQuadraticEquation(double a, double b, double c, double[] target) {
         double p = b / a;
@@ -483,7 +483,7 @@ public class Geo2D {
 
     /**
      * Returns the vertex (apex / scheitelpunkt) [x, y] of the quadratic equation ax^2 + bx + c = y
-     * 
+     *
      * @return
      */
     public static double[] getVertexPointForQuadraticEquation(double a, double b, double c, double[] target) {
@@ -496,10 +496,10 @@ public class Geo2D {
 
     /**
      * The qubic equation has the form of ax^3 + bx^2 + cx + d = y
-     * 
+     *
      * This method finds the parameter a, b, c and d fitting the 4 given points
-     * 
-     * 
+     *
+     *
      */
     public static CubicFunctionParameters getCubicFunctionParameters(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4, CubicFunctionParameters target) {
         /*-
@@ -577,7 +577,7 @@ public class Geo2D {
 
     /**
      * This methods returns an angle between 0 and 2PI which equivalent to the given angle angle
-     * 
+     *
      * @param d
      * @return
      */
@@ -588,7 +588,7 @@ public class Geo2D {
 
     /**
      * This method returns true if the vector p21p22 is bend clockwise when connected to the end to p11p12. This is only true in a screen / image coordinate system where the origin is in the left top corner.
-     * 
+     *
      * NOTE: In an euclidean coordinate system (origin in the left bottom corner) this method would return true if both vectors are orientated anti-clockwise.
      */
 
@@ -604,9 +604,9 @@ public class Geo2D {
 
     /**
      * This method returns true if the 3 given points are orientated clockwise (screen coordinate system)
-     * 
+     *
      * (B - A) x (C - A) > 0
-     * 
+     *
      * U x V = Ux*Vy-Uy*Vx
      */
     public static boolean isClockwise(double p1x, double p1y, double p2x, double p2y, double p3x, double p3y) {
@@ -615,9 +615,9 @@ public class Geo2D {
 
     /**
      * This method returns true if the 3 given points are orientated counter-clockwise (screen coordinate system)
-     * 
+     *
      * (B - A) x (C - A) < 0
-     * 
+     *
      * U x V = Ux*Vy-Uy*Vx
      */
     public static boolean isCounterClockwise(double p1x, double p1y, double p2x, double p2y, double p3x, double p3y) {
@@ -626,9 +626,9 @@ public class Geo2D {
 
     /**
      * This method returns true if the 3 given points are colinear in regard of given epsilon
-     * 
+     *
      * (B - A) x (C - A) = 0
-     * 
+     *
      * U x V = Ux*Vy-Uy*Vx
      */
     public static boolean isColinear(double p1x, double p1y, double p2x, double p2y, double p3x, double p3y, double epsilon) {
@@ -637,9 +637,9 @@ public class Geo2D {
 
     /**
      * This method returns true if the 3 given points are colinear
-     * 
+     *
      * (B - A) x (C - A) = 0
-     * 
+     *
      * U x V = Ux*Vy-Uy*Vx
      */
     public static boolean isColinear(double p1x, double p1y, double p2x, double p2y, double p3x, double p3y) {
@@ -648,90 +648,90 @@ public class Geo2D {
 
     /**
      * This method finds the center line of the given list of points for which the sum of the distance of all points to the line is minimal.
-     * 
+     *
      * The base vector of the center line is always the center of the list of points.
-     * 
+     *
      * The direction vector has always a positive x and the length of the vector is always 1.
      */
     /*-
      * We start with the equation to calculate the minimum distance of a point to a line:
-     * 
+     *
      * ->  Math.abs((y2 - y1) * x - (x2 - x1) * y + x2 * y1 - y2 * x1) / getDistance(x1, y1, x2, y2);
-     * 
+     *
      * getDistance(x1, y1, x2, y2) is linear factor -> drop it
-     *   
-     * ->  Math.abs((y2 - y1) * x - (x2 - x1) * y + x2 * y1 - y2 * x1);  
-     * 
-     * We need our distance to be positive which is insured with Math.abs(). Math.sqr does this as well. Substituting Math.abs with Math.sql allows us 
-     * to transform the equation but in certain conditions leads to less optimal solutions (e.g. a square) 
-     * 
-     * ->  Math.sqr((y2 - y1) * x - (x2 - x1) * y + x2 * y1 - y2 * x1);  
-     * 
+     *
+     * ->  Math.abs((y2 - y1) * x - (x2 - x1) * y + x2 * y1 - y2 * x1);
+     *
+     * We need our distance to be positive which is insured with Math.abs(). Math.sqr does this as well. Substituting Math.abs with Math.sql allows us
+     * to transform the equation but in certain conditions leads to less optimal solutions (e.g. a square)
+     *
+     * ->  Math.sqr((y2 - y1) * x - (x2 - x1) * y + x2 * y1 - y2 * x1);
+     *
      * The points (x1,y1) and (x2,y2) define the center line we are looking for and the point (x,y) stands for a point of our list of points
      * We can set (x2,y2) with the center list of points:
      * x2 -> mx
      * y2 -> my
      * x -> xi
      * y -> yi
-     * 
+     *
      * We are now looking for values for (x1,y1)
-     * 
+     *
      * x1 -> x
      * y1 -> y
-     * 
+     *
      * ->  Math.sqr((y2 - y1) * x - (x2 - x1) * y + x2 * y1 - y2 * x1)
      * ->  Math.sqr((my - y1) * x - (mx - x1) * y + mx * y1 - my * x1)
      * ->  Math.sqr((my - y) * xi - (mx - x) * yi + mx * y - my * x)
-     *  
+     *
      * We are looking for the values (x,y) which minimise the sum of our transformed distance/error function over all given points
-     * 
+     *
      *  -> min = sum(Math.sqr((my - y) * xi - (mx - x) * yi + mx * y - my * x))
-     * 
+     *
      *  we can simplify the equation
-     * 
+     *
      *  -> min = sum(Math.sqr(my * xi - y * xi - mx * yi + x * yi + mx * y - my * x))
      *  -> min = sum(Math.sqr(-y * xi - mx * yi + x * yi + mx * y - my * x + my * xi))
-     *  -> min = sum(Math.sqr(-y * xi + x * yi + mx * y - my * x + my * xi - mx * yi)) 
+     *  -> min = sum(Math.sqr(-y * xi + x * yi + mx * y - my * x + my * xi - mx * yi))
      *
      *  c = my * xi - mx * yi
      *
-     *  -> min = sum(Math.sqr(-y * xi + x * yi + mx * y - my * x + c)) 
+     *  -> min = sum(Math.sqr(-y * xi + x * yi + mx * y - my * x + c))
      *  -> min = sum(Math.sqr(y * (mx-xi) + x * (yi - my) + c))
      *
-     *  substitude: 
+     *  substitude:
      *  -> mxi = mx-xi
      *  -> myi = yi - my
-     * 
-     *  -> min = sum(Math.sqr(y * mxi + x * myi + c)) 
+     *
+     *  -> min = sum(Math.sqr(y * mxi + x * myi + c))
      *  -> min = sum(y*y*mxi*mxi + 2*y*mxi*x*myi + 2*y*mxi*c + x*x*myi*myi + 2*x*myi*c + c*c)
      *  -> min = y*y*sum(mxi*mxi) + x*x*sum(myi*myi) + x*y*sum(2*mxi*myi) + y*sum(2*mxi*c) + x*sum(2*myi*c) + sum(c*c)
-     *  
+     *
      * sum(c*c) is a constant which does not affect the result so we can drop it (or we keep it and later it will be dropped by the derivative)
      *
      *  -> min = y*y*sum(mxi*mxi) + x*x*sum(myi*myi) + x*y*sum(2*mxi*myi) + y*sum(2*mxi*c) + x*sum(2*myi*c)
      * more substitutions:
-     * 
+     *
      *  -> smximxi = sum(mxi*mxi)
      *  -> smyimyi = sum(myi*myi)
      *  -> mximyi2 = sum(2*mxi*myi)
      *  -> mxic2 = sum(2*mxi*c)
      *  -> myic2 = sum(2*myi*c)
-     *  
+     *
      *  -> min = y*y*smximxi + x*x*smyimyi + x*y*mximyi2 + y*mxic2 + x*myic2
-     *  
+     *
      * We have an optimising problem with 2 unknowns. However, y depends on x so we can just pick any x to calculate a matching y.
-     * We could pick the simplest value 0 but mx might be 0 already so we pick mx+1. 
-     * 
+     * We could pick the simplest value 0 but mx might be 0 already so we pick mx+1.
+     *
      * with x = mx+1 = x
      *  -> min = y*y*smximxi + y*mximyi2*x + y*mxic2 + x*x*smyimyi + x*myic2
      *  -> min = y*y*smximxi + y*(mximyi2*x + mxic2) + x*x*smyimyi + x*myic2
-     *  
+     *
      * We have now an optimising problem with 1 unknown of a quadratic equation. The equation has its minimum where its derivative is 0
      * So we need to take the derivative and calculate where it is 0 (Nullstelle)
-     * 
-     *  -> 0 = 2*y*smximxi + mximyi2*x + mxic2 
+     *
+     *  -> 0 = 2*y*smximxi + mximyi2*x + mxic2
      *  -> y = -(mximyi2*x + mxic2) / (2 * smximxi)
-     * 
+     *
      */
     public static Line findCenterLine(double[] points, Line result) {
         int count = points.length / 2;
